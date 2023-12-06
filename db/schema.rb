00c3fe319a2a21c3756fdf41a9bd82171ee82d06
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-12-06 16:09:25 +0000

    [fix] テーブルにindex追加

--- db/schema.rb
+++ db/schema.rb
@@ -151,6 +151,7 @@ ActiveRecord::Schema.define(version: 2023_12_06_092938) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
@@ -169,6 +170,7 @@ ActiveRecord::Schema.define(version: 2023_12_06_092938) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
