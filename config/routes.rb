2c844c5e87a5ed0883af1424e8576dc8f2bb1982
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-12-07 14:07:52 +0000

    [add] routesの記述(途中)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'homes/top'
-  end
+
   devise_for :users, controllers: {
     registrations: "user/registrations",
     sessions: "user/sessions"
@@ -9,42 +7,38 @@ Rails.application.routes.draw do
   devise_for :admin, controllers: {
     sessions: "admin/sessions"
   }
-  
+
+  # deviseとかぶるためmenbersにurlを変更
+  scope module: :user do
+    root to: 'homes#top'
+    resources :members, only: [:index, :show, :update], controller: 'users' do
+      collection do
+        patch :cancellation
+      end
+      resources :cats, only: [:index, :create, :update, :destroy]
+      resources :posts, only: [:index, :show, :create, :update, :destroy] do
+        resources :comments, only: [:create, :destroy]
+        resources :favorites, only: [:index, :create, :destroy]
+        resources :bookmarks, only: [:index, :create, :destroy]
+      end
+      resources :items, only: [:index, :create, :update, :destroy]
+    end
+  end
   namespace :user do
     get 'rooms/show'
   end
-  namespace :user do
-    get 'posts/index'
-    get 'posts/show'
-  end
-  namespace :user do
-    get 'bookmarks/index'
-  end
-  namespace :user do
-    get 'favorites/index'
-  end
-  namespace :user do
-    get 'cats/index'
-  end
-  namespace :user do
-    get 'items/index'
-  end
-  namespace :user do
-    get 'users/index'
-    get 'users/show'
-  end
+
   namespace :admin do
-    get 'posts/index'
-    get 'posts/show'
+    root to: 'homes#top'
+    resources :breeds, only: [:index, :create, :update, :destroy]
+    resources :users, only: [:index, :show, :update] do
+      collection do
+        patch :cancellation
+      end
+      resources :posts, only: [:index, :show, :destroy] do
+        resources :comments, only: [:destroy]
+      end
+    end
   end
-  namespace :admin do
-    get 'breeds/index'
-  end
-  namespace :admin do
-    get 'users/index'
-    get 'users/show'
-  end
-  namespace :user do
-    get 'homes/top'
-  end
+
 end
