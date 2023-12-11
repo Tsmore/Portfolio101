c87f39088617fd960e5b5f158a3583a38e262e66
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-12-11 12:49:26 +0000

    [update] flash.jsの記述変更

--- app/javascript/packs/flash.js
+++ app/javascript/packs/flash.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('turbolinks:load', function() {
   const flashMessages = document.querySelectorAll('.flash');
 
   flashMessages.forEach(function(flashMessage) {
     setTimeout(function() {
       flashMessage.style.display = 'none';
-    }, 5000);
+    }, 5000); // 5秒後に非表示
   });
 });
