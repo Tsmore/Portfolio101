80d08f4833315c9e74cc6f99b29fa7b60d424b88
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-12-13 09:20:26 +0000

    [add] タグ検索機能(タグをリンク＋タグの文字検索)

--- app/controllers/user/all_posts_controller.rb
+++ app/controllers/user/all_posts_controller.rb
@@ -1,5 +1,17 @@
 class User::AllPostsController < ApplicationController
   def index
-    @posts = Post.all
+    if params[:tag]
+      # @tagに検索ワードを代入
+      @tag = Tag.find_by(name: params[:tag])
+      # @tagがnilじゃないなら@tag.postsで表示,@tagがnilなら空のクエリセット(Post.none)でアラート
+      if @tag
+        @posts = @tag.posts
+      else
+        flash.now[:alert] = "指定されたタグは存在しません"
+        @posts =  Post.none
+      end
+    else
+      @posts = Post.all
+    end
   end
 end
