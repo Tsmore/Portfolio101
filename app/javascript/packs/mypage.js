f505e5c919f8513a61f4b3336db8330bab08df16
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-12-18 07:32:32 +0000

    [update] マイページ一旦完成

---
+++ app/javascript/packs/mypage.js
@@ -0,0 +1,32 @@
+document.addEventListener('turbolinks:load', () => {
+  const mypageSection = document.getElementById('mypage-section');
+  const postsSection = document.getElementById('posts-section');
+  const mypageButton = document.getElementById('show-mypage');
+  const postsButton = document.getElementById('show-posts');
+
+  mypageButton.addEventListener('click', function() {
+    postsSection.classList.remove('active');
+    postsSection.style.display = 'none';
+
+    mypageSection.style.display = 'block';
+    setTimeout(() => {
+      mypageSection.classList.add('active');
+    }, 100);
+
+    mypageButton.classList.add('active');
+    postsButton.classList.remove('active');
+  });
+
+  postsButton.addEventListener('click', function() {
+    mypageSection.classList.remove('active');
+    mypageSection.style.display = 'none';
+
+    postsSection.style.display = 'block';
+    setTimeout(() => {
+      postsSection.classList.add('active');
+    }, 100);
+
+    postsButton.classList.add('active');
+    mypageButton.classList.remove('active');
+  });
+});
