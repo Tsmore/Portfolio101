9f852dfe776675d61e732951814514eddfd4d7aa
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-12-18 10:10:04 +0000

    [update] image_previewの編集(猫の編集では表示されない..)

--- app/javascript/packs/image-preview.js
+++ app/javascript/packs/image-preview.js
@@ -1,10 +1,12 @@
-document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById('user_image').addEventListener('change', function(event){
-    var output = document.getElementById('image_preview');
-    output.src = URL.createObjectURL(event.target.files[0]);
-    output.onload = function() {
-      URL.revokeObjectURL(output.src) // 画像読み込み後にメモリ解放
-    }
-    output.style.display = 'block';
+document.addEventListener('turbolinks:load', function() {
+  document.querySelectorAll('.image_upload').forEach(function(input) {
+    input.addEventListener('change', function(event) {
+      var output = document.getElementById('image_preview');
+      output.src = URL.createObjectURL(event.target.files[0]);
+      output.onload = function() {
+        URL.revokeObjectURL(output.src); // 画像読み込み後にメモリ解放
+      }
+      output.style.display = 'block';
+    });
   });
 });
