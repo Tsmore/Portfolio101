39da1a1b3e9928ddfeba9d96be004d06966c4592
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-12-18 09:44:11 +0000

    [update] マイ猫ページ完成

---
+++ app/javascript/packs/edit-cat.js
@@ -0,0 +1,21 @@
+document.addEventListener('turbolinks:load', () => {
+  const userId = document.querySelector('.container.body').getAttribute('data-user-id');
+  document.querySelectorAll('.edit-cat-btn').forEach(button => {
+    button.addEventListener('click', () => {
+      const catId = button.getAttribute('data-cat-id');
+      const catName = button.getAttribute('data-cat-name');
+      const catIntroduction = button.getAttribute('data-cat-introduction');
+      const catBreedId = button.getAttribute('data-cat-breed-id');
+      const catSex = button.getAttribute('data-cat-sex');
+      const catBirth = button.getAttribute('data-cat-birth');
+
+      const form = document.querySelector('#edit-cat-form');
+      form.action = `/users/${userId}/cats/${catId}`;
+      form.querySelector('[name="cat[name]"]').value = catName;
+      form.querySelector('[name="cat[introduction]"]').value = catIntroduction;
+      form.querySelector('[name="cat[breed_id]"]').value = catBreedId;
+      form.querySelector('[name="cat[sex]"]').value = catSex;
+      form.querySelector('[name="cat[date_of_birth]"]').value = catBirth;
+    });
+  });
+});
