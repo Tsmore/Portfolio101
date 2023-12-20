f6e7e4a62188c28948665e83c4be8abd5b21b8d3
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-12-20 10:23:27 +0000

    [update] 切り替え完了

---
+++ app/javascript/packs/following-followers.js
@@ -0,0 +1,22 @@
+document.addEventListener('turbolinks:load', () => {
+  const followingSection = document.getElementById('following-results-section');
+  const followersSection = document.getElementById('followers-results-section');
+  const followingButton = document.getElementById('following-results');
+  const followersButton = document.getElementById('followers-results');
+
+  followingButton.addEventListener('click', function() {
+    followersSection.classList.remove('active');
+    followingSection.classList.add('active');
+
+    followingButton.classList.add('active');
+    followersButton.classList.remove('active');
+  });
+
+  followersButton.addEventListener('click', function() {
+    followingSection.classList.remove('active');
+    followersSection.classList.add('active');
+
+    followersButton.classList.add('active');
+    followingButton.classList.remove('active');
+  });
+});
