576fe0558a52ee435e3cc01df61947446e3eedd4
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-12-23 16:12:35 +0000

    [update] sidebarにログイン、レイアウト微修正

--- app/javascript/packs/top-page.js
+++ app/javascript/packs/top-page.js
@@ -58,7 +58,7 @@ document.addEventListener('turbolinks:load', () => {
   const wrapper = document.querySelector('.wrapper');
   const loginLink = document.querySelector('.login-link');
   const registerLink = document.querySelector('.register-link');
-  const btnPopup = document.querySelector('.login-popup-btn');
+  const btnPopups = document.querySelectorAll('.login-popup-btn');
   const iconClose = document.querySelector('.icon-close');
 
 
@@ -70,8 +70,11 @@ document.addEventListener('turbolinks:load', () => {
     wrapper.classList.remove('active');
   });
 
-  btnPopup.addEventListener('click', () => {
-    wrapper.classList.add('active-popup');
+  btnPopups.forEach(btn => {
+    btn.addEventListener('click', (event) => {
+      event.stopPropagation();
+      wrapper.classList.add('active-popup');
+    });
   });
 
   iconClose.addEventListener('click', () => {
