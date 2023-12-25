38e124ea35f4cbc765ef376cbfc5a5981a1b9f3b
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-12-25 16:15:16 +0000

    [add] 投稿一覧、詳細削除、投稿についてるコメント削除一旦完成

--- app/controllers/admin/all_posts_controller.rb
+++ app/controllers/admin/all_posts_controller.rb
@@ -1,4 +1,19 @@
 class Admin::AllPostsController < ApplicationController
+
   def index
+    @posts = Post.page(params[:page]).per(12)
+  end
+
+  def show
+
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      redirect_to admin_posts_path, notice: "投稿を削除しました"
+    else
+      redirect_to admin_posts_path, alert: "投稿の削除に失敗しました"
+    end
   end
 end
--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -3,5 +3,13 @@ class Admin::PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
+    @user = User.find(params[:user_id])
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to admin_all_posts_path, notice: "投稿を削除しました"
   end
 end
