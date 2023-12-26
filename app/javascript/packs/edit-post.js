d23af71577631d00e17b088753cca2d25d81a4b7
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-12-26 07:46:30 +0000

    [fix] CSRFトークン隠しフィールド

--- app/javascript/packs/edit-post.js
+++ app/javascript/packs/edit-post.js
@@ -1,4 +1,7 @@
 document.addEventListener('turbolinks:load', () => {
+  // CSRFトークンの値を取得
+  const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
   const container = document.querySelector('.container.body');
   if (container) {
     const userId = container.getAttribute('data-user-id');
@@ -13,6 +16,8 @@ document.addEventListener('turbolinks:load', () => {
         const form = document.querySelector('#edit-post-form');
         if (form) {
           form.action = `/users/${userId}/posts/${postId}`;
+
+          // フォームフィールドに値を設定
           const titleField = form.querySelector('[name="post[title]"]');
           const bodyField = form.querySelector('[name="post[body]"]');
           const tagsField = form.querySelector('[name="post[tag_list]"]');
@@ -20,6 +25,13 @@ document.addEventListener('turbolinks:load', () => {
           if (titleField) titleField.value = postTitle;
           if (bodyField) bodyField.value = postBody;
           if (tagsField) tagsField.value = postTags;
+
+          // CSRFトークンを隠しフィールドとしてフォームに追加
+          const hiddenInput = document.createElement('input');
+          hiddenInput.type = 'hidden';
+          hiddenInput.name = 'authenticity_token';
+          hiddenInput.value = csrfToken;
+          form.appendChild(hiddenInput);
         }
       });
     });
