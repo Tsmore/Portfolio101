9f16aa3587fbacf0786eda759b881b9404da7cbb
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-12-26 17:33:28 +0000

    [fix] all_commentsを最新順に

--- app/controllers/admin/all_comments_controller.rb
+++ app/controllers/admin/all_comments_controller.rb
@@ -1,6 +1,6 @@
 class Admin::AllCommentsController < ApplicationController
 
   def index
-    @comments = Comment.page(params[:page]).per(12)
+    @comments = Comment.order(created_at: :desc).page(params[:page]).per(12)
   end
 end
