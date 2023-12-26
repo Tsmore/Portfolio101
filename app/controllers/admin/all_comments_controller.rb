2b2a5ca6bb79998db04148a67f730caadbe04501
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-12-26 17:30:44 +0000

    [add] adminでコメント一覧緊急追加

--- app/controllers/admin/all_comments_controller.rb
+++ app/controllers/admin/all_comments_controller.rb
@@ -1,4 +1,6 @@
 class Admin::AllCommentsController < ApplicationController
+
   def index
+    @comments = Comment.page(params[:page]).per(12)
   end
 end
--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,5 +1,9 @@
 class Admin::CommentsController < ApplicationController
 
+  def index
+    @comments = Comment.all
+  end
+
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
