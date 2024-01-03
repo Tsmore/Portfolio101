af4cefd8f532e10e26d125ea39645febb539d8df
Tsmore <143774064+Tsmore@users.noreply.github.com>
2024-01-03 14:21:53 +0000

    [update] sidebarをホバーで展開

--- app/javascript/packs/sidebar.js
+++ app/javascript/packs/sidebar.js
@@ -2,20 +2,30 @@ document.addEventListener('turbolinks:load', () => {
 
   const sidebar = document.querySelector('.sidebar');
 
+  // クリックで展開
   // aタグは処理をキャンセル
-  sidebar.addEventListener('click', function(event) {
-    if (event.target.tagName === 'A' ) {
-      return;
-    }
-    if (!sidebar.classList.contains('active')) {
-      sidebar.classList.add('active');
-    }
-    event.stopPropagation();
+  // sidebar.addEventListener('click', function(event) {
+  //   if (event.target.tagName === 'A' ) {
+  //     return;
+  //   }
+  //   if (!sidebar.classList.contains('active')) {
+  //     sidebar.classList.add('active');
+  //   }
+  //   event.stopPropagation();
+  // });
+
+  // document.addEventListener('click', function(event) {
+  //   if (!sidebar.contains(event.target)) {
+  //     sidebar.classList.remove('active');
+  //   }
+  // });
+
+  // ホバーで展開
+  sidebar.addEventListener('mouseenter', function(event) {
+    sidebar.classList.add('active');
   });
 
-  document.addEventListener('click', function(event) {
-    if (!sidebar.contains(event.target)) {
-      sidebar.classList.remove('active');
-    }
+  sidebar.addEventListener('mouseleave', function(event) {
+    sidebar.classList.remove('active');
   });
 });
